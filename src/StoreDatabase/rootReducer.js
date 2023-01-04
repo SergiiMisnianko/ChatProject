08fffe703baf659460f745bc7966d28199eb5c5e
Sergii <mysnianko@gmail.com>
2023-01-04 17:43:21 +0000

    Update rootReducer.js

--- src/StoreDatabase/rootReducer.js
+++ src/StoreDatabase/rootReducer.js
@@ -23,10 +23,6 @@ export function rootReducer(state = initialState, action) {
           {
             done: false, text: action.payload, avatar: "https://vraki.net/sites/default/files/mood/29_9.jpg", enemyname: "I`m",
           },
-          { done: false, text: "Is this really a human?!!", avatar: "https://vraki.net/sites/default/files/inline/images/1_102.jpg", enemyname: "Robot-2", },
-          { done: false, text: "The elimination team is already moving forward!", avatar: "https://vraki.net/sites/default/files/inline/images/14_74.jpg", enemyname: "Robot-3", },
-          { done: false, text: "Horrible! Again humans!!!", avatar: "https://vraki.net/sites/default/files/inline/images/22_72.jpg", enemyname: "Robot-4", },
-          { done: false, text: "Stop! Everything is not so clear!", avatar: "https://vraki.net/sites/default/files/inline/images/26_74.jpg", enemyname: "Robot-5", },
         ]
       };
 
