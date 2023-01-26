258e4f002aaa4f4fa926e564bdab71677822cdd9
Sergii <mysnianko@gmail.com>
2023-01-26 20:06:01 +0000

    Change footer

--- src/ChatMainPage/MainPageFooter.js
+++ src/ChatMainPage/MainPageFooter.js
@@ -6,6 +6,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MoreIcon from '@mui/icons-material/MoreVert';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import Typography from '@mui/material/Typography';
 
 export { Footer }
 
@@ -21,28 +23,38 @@ export default function BottomAppBar() {
 
             <CssBaseline />
 
-            <AppBar 
-            position="fixed"
-            color="primary" 
-            sx={{ top: 'auto', bottom: 0, mt: 5 }}>
+            <AppBar
+                position="fixed"
+                color="primary"
+                sx={{ top: 'auto', bottom: 0, mt: 5 }}>
 
-                <Toolbar>
-
-                    <IconButton color="secondary" >
-                        <NotificationsActiveIcon />
-                    </IconButton>
-
-
-                    <Box sx={{ flexGrow: 1 }} />
-
-                    <IconButton color="secondary">
-                        <MoreIcon />
-                    </IconButton>
-
-                </Toolbar>
+                <IconButton color="secondary" >
+                    <CopyrightIcon />  
+                    <Typography variant="h6">Sergii Misnianko</Typography>
+                </IconButton>
+   
             </AppBar>
 
         </React.Fragment>
     );
 }
 
+/*
+ <Toolbar>
+
+                    <IconButton color="secondary" >
+                        <NotificationsActiveIcon />
+                    </IconButton>
+
+
+                    <Box sx={{ flexGrow: 1 }}>
+                    </Box>
+
+
+                    <IconButton color="secondary">
+                        <MoreIcon />
+                    </IconButton>
+
+                </Toolbar>
+
+*/
